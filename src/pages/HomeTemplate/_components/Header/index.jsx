8e4a96dd9e88387007d4710022258e731aa007c2
Kayson <kietfox123@gmail.com>
2025-08-18 19:34:59 +0700

    kiet

--- src/pages/HomeTemplate/_components/Header/index.jsx
+++ src/pages/HomeTemplate/_components/Header/index.jsx
@@ -86,8 +86,9 @@ export default function Header() {
                 className={({ isActive }) =>
                   isActive
                     ? "my-active"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "
                 }
+
               >
                 Order Movie
               </NavLink>
